afecdbd921356cd9672fc920b2a3cefa666dbb61
SlimRunner <dv.polygon@gmail.com>
2024-08-12 01:48:39 -0700

    Add unused utility (consider regression)

--- include/Algorithms.hpp
+++ include/Algorithms.hpp
@@ -15,4 +15,19 @@ enum class Dir8 {
   WEST = 1 << 8,
 };
 
+struct NeighborMark {
+  using IterInt = PseudoIter<int>;
+  int mark = static_cast<int>(Dir8::NONE);
+
+  int operator&(const Dir8 &rhs) { return mark & static_cast<int>(rhs); }
+
+  NeighborMark &operator|=(const Dir8 &rhs) {
+    mark |= static_cast<int>(rhs);
+    return *this;
+  }
+
+  IterInt begin() { return IterInt(0); }
+  IterInt end() { return IterInt(9); }
+};
+
 } // namespace alg
