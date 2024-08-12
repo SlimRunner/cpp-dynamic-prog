f5e9ef0fdc12a866838469d27366a1b6f70fcf7f
SlimRunner <dv.polygon@gmail.com>
2024-08-12 01:49:43 -0700

    Implement sequence scoring

--- include/Algorithms.hpp
+++ include/Algorithms.hpp
@@ -30,4 +30,44 @@ struct NeighborMark {
   IterInt end() { return IterInt(9); }
 };
 
+template <class Seq, class Elem, class Num>
+void sequenceScoringNW(const Seq &seq1, const Seq &seq2, MutMatrix<Num> &scores,
+                       MutMatrix<Num> &btrace,
+                       std::function<Num(Elem, Elem)> matchPenalty,
+                       std::function<Num()> gapPenalty) {
+  static_assert(
+      std::is_same<decltype(std::declval<Seq>().at(0)), Elem &>::value ||
+          std::is_same<decltype(std::declval<Seq>().at(0)),
+                       const Elem &>::value,
+      "Container Seq must support 'at()' method returning Elem or const Elem "
+      "reference");
+  const size_t N = scores.rowSize();
+  const size_t M = scores.colSize();
+  Num left, above, diag, maxScore;
+
+  for (size_t i = 1; i < N; ++i) {
+    scores(i, 0) = -i;
+  }
+  for (size_t j = 1; j < N; ++j) {
+    scores(0, j) = -j;
+  }
+
+  for (size_t i = 1; i < N; ++i) {
+    for (size_t j = 1; j < M; ++j) {
+      left = scores(i, j - 1) + gapPenalty();
+      above = scores(i - 1, j) + gapPenalty();
+      diag =
+          scores(i - 1, j - 1) + matchPenalty(seq1.at(i - 1), seq2.at(j - 1));
+      maxScore = std::max({left, above, diag});
+      scores(i, j) = maxScore;
+      btrace(i - 1, j - 1) |=
+          static_cast<Num>(left == maxScore ? Dir8::WEST : Dir8::NONE);
+      btrace(i - 1, j - 1) |=
+          static_cast<Num>(above == maxScore ? Dir8::NORTH : Dir8::NONE);
+      btrace(i - 1, j - 1) |=
+          static_cast<Num>(diag == maxScore ? Dir8::NORTH_WEST : Dir8::NONE);
+    }
+  }
+}
+
 } // namespace alg
